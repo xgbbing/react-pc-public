8a72479391f544a19f8126d9af399fae497f2937
Alice_Xu <xgb.xu@qq.com>
2026-06-05 16:56:12 +0800

    feat: 接入前端应用监控sdk

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,28 @@
 // 运行时配置
 import HeaderCity from '@/components/HeaderCity';
 import { useModel } from '@umijs/max';
+import {
+  ApiPlugin,
+  JsErrorPlugin,
+  Monitor,
+  PVPlugin,
+  PerformancePlugin,
+  ResourceErrorPlugin,
+} from 'auto-log-sdk';
+
+// 初始化前端监控 SDK
+const monitor = new Monitor({
+  appId: 'react-pc-public',
+  plugins: [
+    [ApiPlugin, {}],
+    [ResourceErrorPlugin, {}],
+    [JsErrorPlugin, {}],
+    [PVPlugin, {}],
+    [PerformancePlugin, {}],
+  ],
+});
+
+monitor.install();
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
