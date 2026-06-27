528a31ede2a33360c90db31b8d234ca6a8a12a0b
Alice_Xu <xgb.xu@qq.com>
2026-06-27 15:19:20 +0800

    feat: 乱七八糟功能

--- src/pages/TabsPage/components/TabsContent.tsx
+++ src/pages/TabsPage/components/TabsContent.tsx
@@ -2,16 +2,48 @@ import image3 from '@/assets/image3.png';
 import image4 from '@/assets/image4.png';
 import useCommonInfo from '@/context/commonContext/useCommonInfo';
 import { getUrlParameter } from '@/utils/format';
-import { Button, Space } from 'antd';
+import { useDebounceFn, useMemoizedFn, useRequest } from 'ahooks';
+import { Button, message, Space } from 'antd';
 import SliderCaptcha from 'rc-slider-captcha';
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 const TabsContent: React.FC = () => {
   const { state, updatedStepsKey } = useCommonInfo();
-
+  const text = 'Hello, world!';
   // 使用示例
   const key = getUrlParameter('key');
   console.log(key);
+
+  const { run: runRequest } = useRequest(
+    async () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('success');
+        }, 1000);
+      });
+    },
+    {
+      manual: true,
+      onSuccess: (res) => {
+        console.log(res);
+      },
+    },
+  );
+
+  const onCopy = () => {
+    message.success('复制成功');
+  };
+
+  const create = useMemoizedFn(() => {
+    console.log('create');
+  });
+
+  const { run: runDebounce } = useDebounceFn(() => create(), { wait: 1000 });
+
+  useEffect(() => {
+    runDebounce();
+    runRequest();
+  }, []);
 
   return (
     <div>
@@ -42,6 +74,12 @@ const TabsContent: React.FC = () => {
             return Promise.resolve();
           }}
         />
+      </div>
+
+      <div>
+        <CopyToClipboard text={text} onCopy={onCopy}>
+          <Button>复制</Button>
+        </CopyToClipboard>
       </div>
     </div>
   );
