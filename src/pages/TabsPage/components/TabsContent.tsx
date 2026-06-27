41a5be7d1dd4c4dfb8c132216d970ae68388ec83
Alice_Xu <xgb.xu@qq.com>
2026-06-27 14:57:12 +0800

    feat: 滑块验证吗

--- src/pages/TabsPage/components/TabsContent.tsx
+++ src/pages/TabsPage/components/TabsContent.tsx
@@ -1,9 +1,13 @@
+import image3 from '@/assets/image3.png';
+import image4 from '@/assets/image4.png';
 import useCommonInfo from '@/context/commonContext/useCommonInfo';
 import { Button, Space } from 'antd';
+import SliderCaptcha from 'rc-slider-captcha';
 import React from 'react';
 
 const TabsContent: React.FC = () => {
   const { state, updatedStepsKey } = useCommonInfo();
+
   return (
     <div>
       <Space>
@@ -15,6 +19,25 @@ const TabsContent: React.FC = () => {
           切换为tab1
         </Button>
       </Space>
+
+      <div>滑块验证码</div>
+      <div>
+        <SliderCaptcha
+          request={async () => {
+            return {
+              bgUrl: image3,
+              puzzleUrl: image4,
+            };
+          }}
+          onVerify={async (data) => {
+            console.log(data);
+            if (data.x < 120 || data.x > 140) {
+              return Promise.reject('验证失败');
+            }
+            return Promise.resolve();
+          }}
+        />
+      </div>
     </div>
   );
 };
